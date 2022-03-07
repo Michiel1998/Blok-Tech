c56cbd5eb190778561a7eeb8982b328ca72e80d4
Michiel Schipper <Michiel.schipper14@gmail.com>
2022-03-07 12:32:39 +0100

    final

--- server.js
+++ server.js
@@ -28,4 +28,4 @@ app.get('/register', (req, res) => {
     });
   });
 
-  app.listen(3000, () => console.log('Server on, port: 3000'))
+  app.listen(process.env.PORT || 3000, () => console.log('Server on, port: 3000'))
